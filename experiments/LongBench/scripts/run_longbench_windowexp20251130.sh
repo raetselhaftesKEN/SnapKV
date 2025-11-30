85d3bbc4da4051d03a3e827ad05300e05fc086ee
KEN <83447549+raetselhaftesKEN@users.noreply.github.com>
2025-11-30 19:18:22 +0800

    Create run_longbench_windowexp20251130.sh

---
+++ experiments/LongBench/scripts/run_longbench_windowexp20251130.sh
@@ -0,0 +1,8 @@
+export https_proxy=http://127.0.0.1:7897
+
+
+nohup python pred_snap.py --model mistral-7B-instruct-v0.2 --compress_args_path ablation_c4096_w16_k7_maxpool.json > 20251130w16.log 2>&1 &
+
+nohup python pred_snap.py --model mistral-7B-instruct-v0.2 --compress_args_path ablation_c4096_w32_k7_maxpool.json > 20251130w32.log 2>&1 &
+
+nohup python pred_snap.py --model mistral-7B-instruct-v0.2 --compress_args_path ablation_c4096_w64_k7_maxpool.json > 20251130w64.log 2>&1 &
