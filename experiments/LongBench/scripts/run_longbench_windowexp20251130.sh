628032da972fb10929f1420901bb2f6d0bbb8474
KEN <83447549+raetselhaftesKEN@users.noreply.github.com>
2025-12-01 19:28:48 +0800

    无kv缓存对照组

--- experiments/LongBench/scripts/run_longbench_windowexp20251130.sh
+++ experiments/LongBench/scripts/run_longbench_windowexp20251130.sh
@@ -11,4 +11,8 @@ nohup python pred_snap.py --model mistral-7B-instruct-v0.2 --compress_args_path 
 
 nohup python pred_snap.py --model mistral-7B-instruct-v0.2 --compress_args_path ablation_c2048_w32_k7_maxpool.json > 20251201c2048.log 2>&1 &
 
-nohup python pred_snap.py --model mistral-7B-instruct-v0.2 --compress_args_path ablation_c512_w32_k7_maxpool.json > 20251201c2048.log 2>&1 &
+nohup python pred_snap.py --model mistral-7B-instruct-v0.2 --compress_args_path ablation_c512_w32_k7_maxpool.json > 20251201c512.log 2>&1 &
+
+nohup python pred_snap.py --model mistral-7B-instruct-v0.2 > 20251201nocompress.log 2>&1 &
+
+nohup python pred_snap_nokv.py --model mistral-7B-instruct-v0.2 > 20251201nokv.log 2>&1 &
