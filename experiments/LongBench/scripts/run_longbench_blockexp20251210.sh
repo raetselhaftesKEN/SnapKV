6c0854c06f11c07381277714966e8771dbe2c73d
KEN <83447549+raetselhaftesKEN@users.noreply.github.com>
2025-12-11 20:56:28 +0800

    新实验

--- experiments/LongBench/scripts/run_longbench_blockexp20251210.sh
+++ experiments/LongBench/scripts/run_longbench_blockexp20251210.sh
@@ -11,3 +11,5 @@ nohup python pred_snap.py --model mistral-7B-instruct-v0.2 --compress_args_path 
 nohup python pred_snap.py --model mistral-7B-instruct-v0.2 --compress_args_path ablation_c4096_w64_b1024_k7_maxpool.json > 20251210w64b1024.log 2>&1 &
 
 nohup python pred_snap.py --model mistral-7B-instruct-v0.2 --compress_args_path ablation_c4096_w64_b4096_k7_maxpool.json > 20251210w64b4096.log 2>&1 &
+
+nohup python pred_snap.py --model mistral-7B-instruct-v0.2 --compress_args_path ablation_c1024_w64_b64_k7_maxpool.json > 20251210c1024w64b64.log 2>&1 &
