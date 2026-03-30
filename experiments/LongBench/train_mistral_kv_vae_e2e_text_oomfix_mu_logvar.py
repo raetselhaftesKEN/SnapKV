d8f7572549f9bc4238da3f6666743430da90c8db
KEN <83447549+raetselhaftesKEN@users.noreply.github.com>
2026-03-30 13:25:01 +0800

    Update train_mistral_kv_vae_e2e_text_oomfix_mu_logvar.py

--- experiments/LongBench/train_mistral_kv_vae_e2e_text_oomfix_mu_logvar.py
+++ experiments/LongBench/train_mistral_kv_vae_e2e_text_oomfix_mu_logvar.py
@@ -1150,13 +1150,13 @@ if __name__ == "__main__":
 
 """
 PYTORCH_ALLOC_CONF=expandable_segments:True \
-python train_mistral_kv_vae_e2e_text_oomfix.py \
+python train_mistral_kv_vae_e2e_text_oomfix_mu_logvar.py \
   --model_name_or_path mistralai/mistral-7B-instruct-v0.2 \
   --dataset_path Salesforce/wikitext \
   --dataset_config_name wikitext-103-raw-v1 \
   --dataset_split train \
   --text_column text \
-  --output_dir /home/ymz/SnapKV/SnapKV/experiments/LongBench/mistral_kv_vae_e2e_wikitext \
+  --output_dir /home/ymz/SnapKV/SnapKV/experiments/LongBench/mistral_kv_vae_e2e_wikitext_20260330 \
   --kv_latent_size 64 \
   --vae_hidden_size 512 \
   --split_kv False \
@@ -1174,5 +1174,9 @@ python train_mistral_kv_vae_e2e_text_oomfix.py \
   --gradient_checkpointing True \
   --use_sdpa True \
   --max_length 768 \
-  --max_steps 1000
+  --max_steps 1000 \
+  --latent_stats_log_steps 10 \
+  --latent_hist_save_steps 100 \
+  --latent_hist_max_points 4096 \
+  --latent_hist_dirname latent_stats
 """
