e150838fdcefeb9a1a97362b6ed0865439d3be4b
KEN <83447549+raetselhaftesKEN@users.noreply.github.com>
2026-06-08 16:10:01 +0800

    Update test_tokenizer.py

--- experiments/LongBench/test_tokenizer.py
+++ experiments/LongBench/test_tokenizer.py
@@ -1,5 +1,8 @@
 from transformers import AutoTokenizer
-tok = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2", use_fast=False)
-for i in range(1000):
-    tok("How is the ground truth for fake news established?", return_tensors="pt")
+tok = AutoTokenizer.from_pretrained(
+    "mistralai/Mistral-7B-Instruct-v0.2",
+    use_fast=False,
+    force_download=True,
+    resume_download=False,
+)
 print("ok")
